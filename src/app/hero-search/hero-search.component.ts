d7cb8e528c1d9831167b2850116e2c51acd36a1a
thaidq2 <thaidq2@topica.edu.vn>
2019-06-26 15:32:53 +0700

    search + obvervable pattern

--- src/app/hero-search/hero-search.component.ts
+++ src/app/hero-search/hero-search.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { Hero } from '../Hero';
+import { HeroService } from '../hero.service';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-hero-search',
@@ -9,10 +11,22 @@ import { Hero } from '../Hero';
 })
 export class HeroSearchComponent implements OnInit {
   heroes$: Observable<Hero[]>;
+  private searchTerms = new Subject<string>();
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.heroes$ = this.searchTerms.pipe(
+      // wait 300ms after eache keystroke before considering the term
+      debounceTime(300),
+
+      distinctUntilChanged(),
+
+      switchMap((term: string) => this.heroService.searchHero(term)),
+    );
   }
 
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
 }
